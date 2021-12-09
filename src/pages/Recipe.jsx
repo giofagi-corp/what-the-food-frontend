d3b9901d571b94fb82cc05e5ff9f884ba282e518
clorodino <juanluiscas@gmail.com>
2021-12-09 18:35:53 +0100

    PagesLayout

---
+++ src/pages/Recipe.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Hero from "../components/Hero"
+import RecipeContent from "../components/RecipeContent"
+
+export default function Recipe() {
+    return (
+        <div>
+            <Hero/>
+            <RecipeContent/>
+        </div>
+    )
+}
