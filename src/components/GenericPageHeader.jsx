6aafe013501a0c2820bfaf011489bdae6a18e261
Facundo Saracco <facundosaracco@gmail.com>
2021-12-10 17:55:04 +0100

    generic page header starter

---
+++ src/components/GenericPageHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BackButton from './BackButton';
+import GenericPageTitle from './GenericPageTitle';
+
+export default function GenericPageHeader() {
+    return (
+        <div>
+            <BackButton/>
+            <GenericPageTitle/>
+        </div>
+    )
+}
