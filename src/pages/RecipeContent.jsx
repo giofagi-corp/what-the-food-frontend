34666f96d4866964f077e97e4145640bf6c0bef5
clorodino <juanluiscas@gmail.com>
2021-12-10 10:04:53 +0100

    WIP structure

---
+++ src/pages/RecipeContent.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function RecipeContent() {
+    return (
+        <div>
+            <h2>DURATION</h2>
+            <p>XX min</p>
+            <h2>INGREDIENTS</h2>
+            <ul>
+                <li>ingredient</li>
+            </ul>
+            <h2>STEPS</h2>
+            <ol>
+                <li>Step</li>
+            </ol>
+        </div>
+    )
+}
