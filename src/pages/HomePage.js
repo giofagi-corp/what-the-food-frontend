3eeb4c26b8d71c54159a135616f8e995313a3258
clorodino <juanluiscas@gmail.com>
2021-12-12 17:06:59 +0100

    wip

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,13 @@
+import React from "react"
+import RecipesList from "../components/RecipesList"
+import { Link } from "react-router-dom";
+
+
 function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
+      <RecipesList/>
     </div>
   );
 }
