096d7796204a1f24c48d5e0fe4d20cde7b90fd43
Facundo Saracco <facundosaracco@gmail.com>
2021-12-12 13:05:08 +0100

    big card starter

---
+++ src/components/Notifications.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+export default function Notifications() {
+
+    const [count, setCount] = useState(0);
+ 
+    return (
+        <div className="Counter">
+            <h2>Counter</h2>
+    
+            <p>You clicked {count} times</p>
+    
+            <button onClick={() => setCount(count - 1)}> - </button>
+            <button onClick={() => setCount(count + 1)}> + </button>
+        </div>
+  );
+}
--- src/components/SocialButton.jsx
+++ src/components/SocialButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Box from '@mui/material/Box';
 import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
+import TabContext from '@material-ui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 
