3a3d5504390b3eccdf2960d37d336156e322567b
clorodino <juanluiscas@gmail.com>
2021-12-12 18:37:51 +0100

    wip

--- src/components/RecipesList.jsx
+++ src/components/RecipesList.jsx
@@ -1,44 +1,17 @@
 import React from 'react'
 
-import { useState, useEffect } from "react";
-import axios from "axios";
 import RecipeCard from "../components/RecipeCard"
-/* import AddProject from "./../components/AddProject";
-const API_URI = process.env.REACT_APP_API_URI; */
 
-export default function RecipesList() {
-  const [recipes, setRecipes] = useState([]);
 
-  const getAllProjects = () => {
-    // Get the token from the localStorage
-    //const storedToken = localStorage.getItem("authToken");
-
-    // Send the token through the request "Authorization" Headers
-    // axios
-    //   .get(`${API_URI}/api/projects`, {
-    //     headers: { Authorization: `Bearer ${storedToken}` },
-    //   })
-    //   .then((response) => setProjects(response.data))
-    //   .catch((error) => console.log(error));
-
-    axios
-      .get(`http://localhost:5000/api/recipe/listAllRecipes`)
-      .then((response) => setRecipes(response.data))
-      .catch((error) => console.log(error));
-  };
-
-  // We set this effect will run only once, after the initial render
-  // by setting the empty dependency array - []
-  useEffect(() => {
-    getAllProjects();
-  }, []);
+export default function RecipesList(props) {
+    const {recipes} = props
 
   return (
     <div>
 
-      {recipes.map((recipe) => (
+     {recipes.map((recipe) => (
         <RecipeCard key={recipe._id} {...recipe} />
-      ))}
+      ))} 
     </div>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,28 @@
 import React from "react"
 import RecipesList from "../components/RecipesList"
-import { Link } from "react-router-dom";
+
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 
 function HomePage() {
+  const [recipes, setRecipes] = useState([]);
+
+  const getAllProjects = () => {
+    axios
+      .get(`http://localhost:5000/api/recipe/listAllRecipes`)
+      .then((response) => setRecipes(response.data))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getAllProjects();
+  }, []);
+
   return (
     <div>
       <h1>Home Page</h1>
-      <RecipesList/>
+      <RecipesList recipes={recipes}/>
     </div>
   );
 }
