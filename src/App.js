eb65207a7e1fecbdba5c12daad29a61f0a4ca68a
clorodino <juanluiscas@gmail.com>
2021-12-13 15:21:52 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -9,44 +9,57 @@ import ProfilePage from "./pages/ProfilePage";
 
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
-import PrivateRoute from "./components/PrivateRoute";    // <== IMPORT
-import AnonRoute from "./components/AnonRoute";        // <== IMPORT
+import PrivateRoute from "./components/PrivateRoute"; // <== IMPORT
+import AnonRoute from "./components/AnonRoute"; // <== IMPORT
 
 import TopRecipesPage from "./pages/TopRecipesPage";
 import TopCuisinePage from "./pages/TopCuisinePage";
 import TopIngredientsPage from "./pages/TopIngredientsPage";
 import NotificationsPage from "./pages/NotificationsPage";
 
-
-
-
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
+    return (
+        <div className="App">
+            <Navbar />
 
-      <Switch>      
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/top-recipes" component={TopRecipesPage} />
-        <Route exact path="/top-cuisine" component={TopCuisinePage} />
-        <Route exact path="/top-ingredients" component={TopIngredientsPage} />
-        <Route exact path="/notifications" component={NotificationsPage} />
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/top-recipes" component={TopRecipesPage} />
+                <Route exact path="/top-cuisine" component={TopCuisinePage} />
+                <Route
+                    exact
+                    path="/top-ingredients"
+                    component={TopIngredientsPage}
+                />
+                <Route
+                    exact
+                    path="/notifications"
+                    component={NotificationsPage}
+                />
+                <Route exact path="/profile" component={ProfilePage} />
 
+                {/* 👇 UPDATE THE EXISTING ROUTES 👇  */}
+                <PrivateRoute
+                    exact
+                    path="/projects"
+                    component={ProjectListPage}
+                />
+                <PrivateRoute
+                    exact
+                    path="/projects/:id"
+                    component={ProjectDetailsPage}
+                />
+                <PrivateRoute
+                    exact
+                    path="/projects/edit/:id"
+                    component={EditProjectPage}
+                />
 
-
-
-        {/* 👇 UPDATE THE EXISTING ROUTES 👇  */}
-        <PrivateRoute exact path="/projects" component={ProjectListPage} />
-        <PrivateRoute exact path="/projects/:id" component={ProjectDetailsPage} />
-        <PrivateRoute exact path="/projects/edit/:id" component={EditProjectPage} />
-        
-        
-        <AnonRoute exact path="/signup" component={SignupPage} />
-        <AnonRoute exact path="/login" component={LoginPage} />
-
-      </Switch>
-    </div>
-  );
+                <AnonRoute exact path="/signup" component={SignupPage} />
+                <AnonRoute exact path="/login" component={LoginPage} />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
