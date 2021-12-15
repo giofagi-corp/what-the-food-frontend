9a5f84a3debde1a99f51144124081653d458cea4
clorodino <juanluiscas@gmail.com>
2021-12-15 10:11:05 +0100

    wip

--- src/components/HomeTopList.jsx
+++ src/components/HomeTopList.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom'
 import RecipeCard from "../components/RecipeCard";
 
 export default function RecipesList(props) {
-  console.log("props ---------->", props);
   const { recipes } = props;
 
   return (
