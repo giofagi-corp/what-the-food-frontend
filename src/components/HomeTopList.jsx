763daede7f028c29cc61c627649c17e6cf699763
Facundo Saracco <facundosaracco@gmail.com>
2021-12-15 11:25:04 +0100

    general styling wip

--- src/components/HomeTopList.jsx
+++ src/components/HomeTopList.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from 'react-router-dom'
-
+import Box from '@mui/material/Box';
 import RecipeCard from "../components/RecipeCard";
 
 export default function RecipesList(props) {
@@ -8,10 +8,10 @@ export default function RecipesList(props) {
   const { recipes } = props;
 
   return (
-    <div>
+    <Box sx={{p: '0 24px' }}> 
       {recipes.map((recipe) => (
         <Link><RecipeCard key={recipe._id} {...recipe} /></Link>
       ))}
-    </div>
+    </Box>
   );
 }
