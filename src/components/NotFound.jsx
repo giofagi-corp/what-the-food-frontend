31c7c964dc952e48f02bed79f4c4d0f7c7853d6b
clorodino <juanluiscas@gmail.com>
2022-01-21 19:14:39 +0100

    Not Found Page working

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function NotFound(props) {
 
     return (
-        <div>
+        <div className='RecipePicUpload NotFound' >
+            <img src="https://res.cloudinary.com/dtgwzogvc/image/upload/v1642788591/images/3d-fluency-close-window_vyohtq.png" alt="Not found" />
             <h2>There are no recipes with those ingredients</h2>
         </div>
     )
