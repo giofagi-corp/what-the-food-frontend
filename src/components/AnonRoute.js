7d056987176bfefb877810b99d5f135ee343860f
Juanlu <juanluiscas@gmail.com>
2022-05-19 17:22:10 +0200

    loading added

--- src/components/AnonRoute.js
+++ src/components/AnonRoute.js
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import { AuthContext } from './../context/auth.context'
 import { Redirect, Route } from 'react-router-dom'
+import { LeapFrog } from '@uiball/loaders'
 
 function AnonRoute(props) {
 	const { to, exact, component: Component, ...restProps } = props
@@ -8,7 +9,7 @@ function AnonRoute(props) {
 	const { isLoggedIn, isLoading } = useContext(AuthContext)
 
 	// If the authentication is still loading ⏳
-	if (isLoading) return <p>Loading ...</p>
+	if (!isLoading) return <div className='loading'><LeapFrog size={50} color="#28C699" />Loading</div>
 
 	// If the user is already logged in, redirect him to home page
 	if (isLoggedIn) return <Redirect to='/' />
